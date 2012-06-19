faf451f975e8c80285ea53bc51b4fcbd30911ec7
Craig Sabbey <csabbey@gmail.com>
2012-06-19 15:44:44 -0400

    some error handling cleanup and added api deactivate

--- application/controllers/api/SurveysController.php
+++ application/controllers/api/SurveysController.php
@@ -60,7 +60,8 @@ class SurveysController extends BaseAPIController
         );
 		$xssfilter = false;
         $iNewSurveyid = Survey::model()->insertNewSurvey($aInsertData, $xssfilter);
-        if (!$iNewSurveyid){
+        if (!$iNewSurveyid)
+        {
         	echo 0;
 			return;
         }
@@ -78,23 +79,14 @@ class SurveysController extends BaseAPIController
 	
 	public function actionActivate()
 	{
-		$iSurveyID = (int)$_POST['sid'];
+		$iSurveyID = $this->getSid();
 		Yii::app()->setLang(new Limesurvey_lang('en'));
-		$clang = Yii::app()->lang;
-    	//$group['Arrays'] = $clang->gT('Arrays');
-		
-		if (!$iSurveyID)
-        {
-            echo CJSON::encode(array('success'=>'false'));
-			Yii::app()->end();
-        }
-		
+			
         $surveyInfo = getSurveyInfo($iSurveyID);
 		
-        if (!isset($surveyInfo['active']) || $surveyInfo['active'] == 'Y'){
-            //$this->getController()->error('Survey not active');
-            echo CJSON::encode(array('success'=>'false'));
-			Yii::app()->end();
+        if (!isset($surveyInfo['active']) || $surveyInfo['active'] == 'Y')
+        {
+        	$this->handleError(500, "Survey activity not set or already active.");
 		}
 		
         Yii::app()->loadHelper("admin/activate");
@@ -114,4 +106,129 @@ class SurveysController extends BaseAPIController
 		echo CJSON::encode(array('success'=>'true'));
 		Yii::app()->end();
 	}
+	
+	/**
+	 * Function responsible to deactivate a survey.
+	 *
+	 * @access public
+	 * @param int $iSurveyID
+	 * @return void
+	 */
+	// CRAIG this fails if already deactivated - is that a problem?
+	public function actionDeactivate()
+	{
+		$iSurveyID = $this->getSid();
+	
+		Yii::app()->setLang(new Limesurvey_lang('en'));
+		
+		// CRAIG what is the difference? Do we need both?
+		//$postsid = Yii::app()->request->getPost('sid', $iSurveyID);
+		$postsid = $iSurveyID;
+		
+		$date = date('YmdHis'); //'Hi' adds 24hours+minutes to name to allow multiple deactiviations in a day
+	
+		if (empty($_POST['ok']))
+		{
+			$aData['surveyid'] = $iSurveyID;
+			$aData['date'] = $date;
+			$aData['dbprefix'] = Yii::app()->db->tablePrefix;
+			$aData['step1'] = true;
+		}
+		else
+		{
+			
+			//See if there is a tokens table for this survey
+			if (Yii::app()->db->schema->getTable("{{tokens_{$postsid}}}"))
+			{
+				if (Yii::app()->db->getDriverName() == 'postgre')
+				{
+					$deactivateresult = Yii::app()->db->createCommand()->renameTable($toldtable . '_tid_seq', $tnewtable . '_tid_seq');
+					$setsequence = "ALTER TABLE ".Yii::app()->db->quoteTableName($tnewtable)." ALTER COLUMN tid SET DEFAULT nextval('{{{$tnewtable}}}_tid_seq'::regclass);";
+					$deactivateresult = Yii::app()->db->createCommand($setsequence)->query();
+					$setidx = "ALTER INDEX {{{$toldtable}}}_idx RENAME TO {{{$tnewtable}}}_idx;";
+					$deactivateresult = Yii::app()->db->createCommand($setidx)->query();
+				}
+	
+				$toldtable = "{{tokens_{$postsid}}}";
+				$tnewtable = "{{old_tokens_{$postsid}_{$date}}}";
+	
+				$tdeactivateresult = Yii::app()->db->createCommand()->renameTable($toldtable, $tnewtable);
+	
+				$aData['tnewtable'] = $tnewtable;
+				$aData['toldtable'] = $toldtable;
+			}
+	
+			// IF there are any records in the saved_control table related to this survey, they have to be deleted
+			$result = Saved_control::model()->deleteSomeRecords(array('sid' => $postsid)); //Yii::app()->db->createCommand($query)->query();
+			$oldtable = "{{survey_{$postsid}}}";
+			$newtable = "{{old_survey_{$postsid}_{$date}}}";
+	
+			//Update the auto_increment value from the table before renaming
+			$new_autonumber_start = 0;
+			$query = "SELECT id FROM ".Yii::app()->db->quoteTableName($oldtable)." ORDER BY id desc LIMIT 1";
+			$result = Yii::app()->db->createCommand($query)->query();
+			if ($result->getRowCount() > 0)
+			{
+				foreach ($result->readAll() as $row)
+				{
+					if (strlen($row['id']) > 12) //Handle very large autonumbers (like those using IP prefixes)
+					{
+						$part1 = substr($row['id'], 0, 12);
+						$part2len = strlen($row['id']) - 12;
+						$part2 = sprintf("%0{$part2len}d", substr($row['id'], 12, strlen($row['id']) - 12) + 1);
+						$new_autonumber_start = "{$part1}{$part2}";
+					}
+					else
+					{
+						$new_autonumber_start = $row['id'] + 1;
+					}
+				}
+			}
+	
+			$condn = array('sid' => $iSurveyID);
+			$insertdata = array('autonumber_start' => $new_autonumber_start);
+	
+			$survey = Survey::model()->findByAttributes($condn);
+			$survey->autonumber_start = $new_autonumber_start;
+			$survey->save();
+			if (Yii::app()->db->getDrivername() == 'postgre')
+			{
+				$deactivateresult = Yii::app()->db->createCommand()->renameTable($oldtable . '_id_seq', $newtable . '_id_seq');
+				$setsequence = "ALTER TABLE $newtable ALTER COLUMN id SET DEFAULT nextval('{$newtable}_id_seq'::regclass);";
+				$deactivateresult = Yii::app()->db->createCommand($setsequence)->execute();
+			}
+	
+			$deactivateresult = Yii::app()->db->createCommand()->renameTable($oldtable, $newtable);
+	
+			$insertdata = array('active' => 'N');
+			$survey->active = 'N';
+			$survey->save();
+	
+			$prow = Survey::model()->find('sid = :sid', array(':sid' => $postsid));
+			if ($prow->savetimings == "Y")
+			{
+				$oldtable = "{{survey_{$postsid}_timings}}";
+				$newtable = "{{old_survey_{$postsid}_timings_{$date}}}";
+	
+				$deactivateresult2 = Yii::app()->db->createCommand()->renameTable($oldtable, $newtable);
+				$deactivateresult = ($deactivateresult && $deactivateresult2);
+			}
+	
+			$aData['surveyid'] = $iSurveyID;
+			$aData['newtable'] = $newtable;
+		}
+	
+		//$this->_renderWrappedTemplate('survey', 'deactivateSurvey_view', $aData);
+		$aData["message"] = "Deactivated";
+		echo json_encode($aData);
+	}
+	
+	private function getSid($required = true) {
+		$sid = (int)$_POST['sid'];
+		if ($required && !$sid) {
+			$this->handleError(400, "Missing sid parameter.");
+		}
+		return $sid;		
+	}
+	
 }
--- application/core/BaseAPIController.php
+++ application/core/BaseAPIController.php
@@ -6,21 +6,32 @@ class BaseAPIController extends LSYii_Controller
     {
 		header('Content-type: application/json');
 		
-		
-		
-		if(empty($_POST['username']) || empty($_POST['password'])){
-			echo CJSON::encode(array('success'=>'false', 'message'=>'No credentials provided'));
-			exit;	
+		if(empty($_POST['username']) || !isset($_POST['password'])){
+			$this->handleError(400, "No credentials provided.");
 		}
 		
    		$identity = new UserIdentity(sanitize_user($_REQUEST['username']), $_REQUEST['password']);
 
         if (!$identity->authenticate()){
-        	echo CJSON::encode(array('success'=>'false', 'message'=>'Credentials are wrong'));
-			exit;
+			$this->handleError(400, "Credentials are wrong.");
         }
-		 
-		
-		
 	}
+
+	// tried hooking up yii's error handling - didn't get it to work...
+	public function handleError($httpStatus, $message){
+		$statusHeader = null;
+		switch ($httpStatus) {
+			case 400:
+				$statusHeader = 'HTTP/1.0 400 Bad Request';
+				break;
+			case 500:
+				$statusHeader = 'HTTP/1.0 500 Internal Server Error';
+				break;
+		}
+		header($statusHeader, true, $httpStatus);
+		$output = array("status" => $httpStatus, "message" => $message);
+		echo json_encode($output);
+		exit;		
+	}
+		
 }

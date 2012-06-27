e66e58126d4f29a58a975f35038d4c216af08496
Craig Sabbey <csabbey@gmail.com>
2012-06-27 14:42:02 -0400

    checkpointing answers - about to rearrange

--- application/models/Survey_answers.php
+++ application/models/Survey_answers.php
@@ -1,37 +1,15 @@
 <?php
-/*
- * LimeSurvey
- * Copyright (C) 2007-2011 The LimeSurvey Project Team / Carsten Schmitz
- * All rights reserved.
- * License: GNU/GPL License v2 or later, see LICENSE.php
- * LimeSurvey is free software. This version may have been modified pursuant
- * to the GNU General Public License, and as distributed it includes or
- * is derivative of works licensed under the GNU General Public License or
- * other free or open source software licenses.
- * See COPYRIGHT.php for copyright notices and details.
- *
- *	$Id$
- */
-class Survey_answers extends CActiveRecord{
-	/**
-	 * Returns the static model of Settings table
-	 *
-	 * @static
-	 * @access public
-     * @param string $class
-	 * @return CActiveRecord
-	 */
+
+// returns answers for all answer types as specified in 
+//   Survey_questions::getExternalTypeSql() as type 'A'
+class Survey_answers extends CActiveRecord
+{
+
 	public static function model($class = __CLASS__)
 	{
 		return parent::model($class);
 	}
 
-	/**
-	 * Returns the setting's table name to be used by the model
-	 *
-	 * @access public
-	 * @return string
-	 */
 	public function tableName()
 	{
 		return '{{answers}}';
@@ -39,22 +17,64 @@ class Survey_answers extends CActiveRecord{
 
     function getAnswersForSurveys($iSurveyIDs = array())
     {
-    	$sql = "select a.qid id, a.code, a.answer defaulttext, a.sortorder `order` from {{answers}} a
-    		join {{questions}} q on a.qid=q.qid";
-    	
-    	if ($iSurveyIDs) 
-    	{
-    		$sql .= " where q.sid IN (".implode(", ", $iSurveyIDs).")";
-    	} 
-    	else 
-    	{
-    		$sql .= " join {{surveys}} s on s.sid=q.sid and s.active='Y'";
-    	}
-
-    	return Yii::app()->db->createCommand($sql)->queryAll();    			 
+    	$answerSql = $this->getSimpleAnswersQuery();
+    	$this->joinSurveys($answerSql, $iSurveyIDs);
+    	 
+    	$multiSql = $this->getMultipleChoiceAnswersQuery();
+    	$this->joinSurveys($multiSql, $iSurveyIDs);
+    	 
+    	$yesNoSql = $this->getYesNoAnswersQuery();
+    	$this->joinSurveys($yesNoSql, $iSurveyIDs);
+    	 
+		$unioned = "SELECT * FROM (".
+						$answerSql->getText().
+					" UNION ".
+						$multiSql->getText().
+					" UNION ".
+						$yesNoSql->getText().
+					") a ORDER BY sid, ordering, subordering";
+    	return Yii::app()->db->createCommand($unioned)->queryAll();		 
     }
 
+    private function getSimpleAnswersQuery() 
+    {
+    	$query = Yii::app()->db->createCommand();
+    	$query->select("a.qid id, a.code, a.answer defaulttext, a.sortorder `order`");
+    	$query->from("{{answers}} a");
+    	$query->join("{{questions}} q",  "q.qid=a.qid");
+    	return $query;    	 
+    }
+    
+    private function getMultipleChoiceAnswersQuery()
+    {
+    	$query = Yii::app()->db->createCommand();
+    	$query->select("qid id, q.title code, q.question defaulttext, q.question_order `order`");
+    	$query->from("{{questions}} q");
+    	$query->join("{{questions}} parent ON q.parent_qid=parent.qid AND parent.type='M'");
+    	return $query;
+    }
+    
+    private function getYesNoAnswersQuery()
+    {
+    	$query = Yii::app()->db->createCommand();
+    	$query->select("qid id, q.title code, q.question defaulttext, q.question_order `order`");
+    	$query->from("{{questions}} q");
+    	$query->join("{{questions}} parent ON q.parent_qid=parent.qid AND parent.type='M'");
+    	return $query;
+    }
 
+    private function joinSurveys($query, $iSurveyIDs)
+    {
+    	if ($iSurveyIDs)
+    	{
+    		$query->where("q.sid IN (".implode(", ", $iSurveyIDs).")");
+    	}
+    	else
+    	{
+    		$query->join("{{surveys}} s", "s.sid=q.sid and s.active='Y'");
+    	}
+    }
+    
 }
 
 ?>
